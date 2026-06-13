be96f9da29329a85f83bc59894f8c6b5075e4a2d
Arin Yadav <arinyadav98@gmail.com>
2026-06-13 21:55:42 +0530

    add android resource source

--- build-logic/convention/src/main/kotlin/org/convention/KotlinMultiplatform.kt
+++ build-logic/convention/src/main/kotlin/org/convention/KotlinMultiplatform.kt
@@ -1,5 +1,6 @@
 package org.convention
 
+import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryTarget
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
@@ -47,6 +48,11 @@ internal fun Project.configureKotlinMultiplatform() {
             freeCompilerArgs.add("-Xexpect-actual-classes")
             freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
             freeCompilerArgs.add("-opt-in=kotlin.time.ExperimentalTime")
+        }
+
+        targets.withType(KotlinMultiplatformAndroidLibraryTarget::class.java).configureEach {
+            @Suppress("UnstableApiUsage")
+            experimentalProperties["android.experimental.kmp.enableAndroidResources"] = true
         }
     }
 }
